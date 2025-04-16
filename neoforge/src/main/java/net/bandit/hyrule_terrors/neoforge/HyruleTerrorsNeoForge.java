68c8f8fff3062f8689f55b436fb607b2d8e5bb4e
Bandit Bytes <banditheeler2020@gmail.com>
2025-04-15 22:55:31 -0400

    neo animation not working

--- neoforge/src/main/java/net/bandit/hyrule_terrors/neoforge/HyruleTerrorsNeoForge.java
+++ neoforge/src/main/java/net/bandit/hyrule_terrors/neoforge/HyruleTerrorsNeoForge.java
@@ -1,24 +1,32 @@
 package net.bandit.hyrule_terrors.neoforge;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import mod.azure.azurelib.rewrite.animation.cache.AzIdentityRegistry;
 import mod.azure.azurelib.rewrite.render.item.AzItemRendererRegistry;
+import net.bandit.hyrule_terrors.HyruleTerrorsConfig;
 import net.bandit.hyrule_terrors.HyruleTerrorsMod;
 import net.bandit.hyrule_terrors.item.weapon.client.renderer.BokoblinArmRenderer;
 import net.bandit.hyrule_terrors.registry.ArmorRegistry;
 import net.bandit.hyrule_terrors.registry.ItemRegistry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.event.ModelEvent;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 import static net.bandit.hyrule_terrors.HyruleTerrorsMod.MOD_ID;
 
 
 @Mod(MOD_ID)
+@EventBusSubscriber(modid = HyruleTerrorsMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class HyruleTerrorsNeoForge {
           public HyruleTerrorsNeoForge(IEventBus modEventBus) {
         HyruleTerrorsMod.init();
@@ -26,15 +34,19 @@ public final class HyruleTerrorsNeoForge {
         modEventBus.addListener(this::commonSetup);
     }
     public void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    private void onClientSetup(final FMLClientSetupEvent event) {
+              ArmorRegistry.initClient(MOD_ID);
+    }
+    @SubscribeEvent
+    public static void onModelRegister(ModelEvent.RegisterAdditional event) {
         AzIdentityRegistry.register(
                 ItemRegistry.BOKOBLIN_ARM.get());
     }
     @SubscribeEvent
-    private void onClientSetup(final FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            AzItemRendererRegistry.register(ItemRegistry.BOKOBLIN_ARM.get(), BokoblinArmRenderer::new);
-            ArmorRegistry.initClient(MOD_ID);
-        });
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+              AzItemRendererRegistry.register(ItemRegistry.BOKOBLIN_ARM.get(), BokoblinArmRenderer::new);
     }
 }
 
